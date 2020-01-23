fad68ec6f5dc0e4af891285caa577e47efec3803
root <root@dellys.ddns.net>
2020-01-23 21:26:02 +0100

    J730FXXU5CSG3

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#0 SMP PREEMPT Thu Jan 23 21:01:29 CET 2020"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "android"
+#define LINUX_COMPILER "gcc version 7.5.0 (Linaro GCC 7.5-2019.12) "
